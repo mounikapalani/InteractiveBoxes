f1781df0664d0a00170240ff808f10550bd1eeb3
mounikapalani <mounikapalani@gmail.com>
2025-06-09 13:28:42 +0530

    updates

--- task.js
+++ task.js
@@ -2,8 +2,12 @@ const boxes = document.querySelectorAll('.bundle-box');
 
 boxes.forEach(box => {
   box.addEventListener('click', () => {
-    boxes.forEach(b => b.classList.remove('active'));
+    boxes.forEach(b => {
+      b.classList.remove('active');
+      b.querySelector('.bundle-header').setAttribute('aria-expanded', 'false');
+    });
     box.classList.add('active');
     box.querySelector('input[type="radio"]').checked = true;
+    box.querySelector('.bundle-header').setAttribute('aria-expanded', 'true');
   });
 });
